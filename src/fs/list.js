1a27cce5f02da7384b91b6837decf07109be34ac
Igor Checheurov <chei@yandex.ru>
2024-10-02 13:00:37 +0300

    feat: add function list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+const fs = require('fs');
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        fs.access('files', (error) => {
+            if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
+                throw new Error ("FS operation failed");
+            } else {
+                fs.readdir('files', (error, content) => {
+                    if (err) {
+                        console.log(error);
+                    } else {
+                        for (item of content) {
+                            console.log(item);
+                        }
+                    }
+                })
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await list();
