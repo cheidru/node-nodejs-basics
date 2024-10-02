25c7eca60f723712a9e9be4219675af31781224e
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-10-02 11:27:40 +0300

    feat: add function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
 const copy = async () => {
-    // Write your code here 
+    // Write your code here
+    const filesFolder = 'files';
+    const copyFolder = 'files_copy';
+    // const copyFolderPath = '\files_copy\\';
+
+    try {
+        // check if 'files' folder exists
+        fs.access(filesFolder, (error) => {
+            if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
+                throw new Error ("FS operation failed");
+            } else {
+                // check if 'files_copy' folder exists
+                fs.access(copyFolder, (error) => {
+                    if (error.code === 'ENOENT') {
+                        // create 'files_copy' folder
+                        fs.mkdir(copyFolder, (err) => {console.log(err)});
+                        // copy files and folders
+                        fs.cp(filesFolder, copyFolder);
+                    } else {
+                        throw new Error ("FS operation failed");                
+                    };
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -3,14 +3,18 @@ const fs = require('fs');
 const create = async () => {
     // Write your code here
     let meFresh = 'I am fresh and young';
-    // check if Files folder and fresh.txt exist
-    fs.open('files/fresh.txt', r, (error) => {
-        if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
-            fs.writeFile('/files/fresh.txt', meFresh, (error) => {console.log(error)})
-        } else {
-            throw new Error("FS operation failed");
-        }
-    })
+    try {
+        // check if Files folder and fresh.txt exist
+        fs.open('files/fresh.txt', r, (error) => {
+            if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
+                fs.writeFile('/files/fresh.txt', meFresh, (error) => {console.log(error)})
+            } else {
+                throw new Error("FS operation failed");
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await create();
