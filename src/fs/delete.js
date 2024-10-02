48269e03bc8bf9d037f0ee33169607415e0b1f16
Igor Checheurov <chei@yandex.ru>
2024-10-02 12:52:30 +0300

    feat: add function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+const fs = require('fs');
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        const fileToDlelete = 'fileToRemove.txt';
+        // Write your code here
+        // check if Files folder exists
+        fs.access(fileToDlelete, (error) => {
+            if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
+                fs.unlink(fileToDlelete, (error) => {console.log(error)});
+            } else {
+                throw new Error ("FS operation failed");
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await remove();
