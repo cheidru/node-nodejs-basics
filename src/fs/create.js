2f1c503d7d5980b2c84c663a7e3b66a1d5525abc
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-10-02 09:30:49 +0300

    feat: create function added

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    let meFresh = 'I am fresh and young';
+    // check if Files folder and fresh.txt exist
+    fs.open('files/fresh.txt', r, (error) => {
+        if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
+            fs.writeFile('/files/fresh.txt', meFresh, (error) => {console.log(error)})
+        } else {
+            throw new Error("FS operation failed");
+        }
+    })
 };
 
 await create();
