66471751b5a11734b7395b6067c15ddd305eb710
Igor Checheurov <chei@yandex.ru>
2024-10-02 13:49:51 +0300

    feat: add function parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    let arr = process.argv.slice(2).map(item => item.replace('--',''));
+    let argsString = [];
+    for(let i=0; i< arr.length; i +=2) {
+        argsString.push(arr[i] + ' is ' + arr[i+1]);
+    }
+
+    console.log(argsString.join(', '));
 };
 
 parseArgs();
