944041437b2e9b81e32f644b34fc0b570536e46e
Igor Checheurov <chei@yandex.ru>
2024-10-02 13:10:56 +0300

    feat: add function read

--- src/fs/list.js
+++ src/fs/list.js
@@ -7,13 +7,10 @@ const list = async () => {
             if (error.code === 'ENOENT') { // ENOENT = 'No such file or directory'
                 throw new Error ("FS operation failed");
             } else {
-                fs.readdir('files', (error, content) => {
-                    if (err) {
-                        console.log(error);
-                    } else {
-                        for (item of content) {
-                            console.log(item);
-                        }
+                fs.readdir('files', (err, content) => {
+                    if (err) throw err;
+                    for (item of content) {
+                        console.log(item);
                     }
                 })
             }
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+const fs = require('fs');
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    fs.access('files/fileToRead.txt', (error) => {
+        if (error) {
+            throw new Error ("FS operation failed");
+        } else {
+            fs.readFile('files/fileToRead.txt', (err, data) => {
+                if(err) throw err;
+                console.log(data);
+            })
+        }
+    });
 };
 
 await read();
