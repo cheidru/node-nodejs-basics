a5615da6d3476846912885e31cea38d838245676
Igor Checheurov <chei@yandex.ru>
2024-10-02 16:29:20 +0300

    feat: start coding hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+const crypto = require('crypto');
+
+// https://www.techiedelight.com/generate-sha-256-hash-javascript/
+// https://nodejs.org/dist/latest-v22.x/docs/api/crypto.html#cryptocreatehashalgorithm-options
+// https://nodejs.org/dist/latest-v22.x/docs/api/crypto.html#hashupdatedata-inputencoding
+// https://nodejs.org/api/stream.html
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const sha256Hash = crypto.createHash('sha256');
+
+    sha256Hash.update(/* Содержимое файла */);
+
+    const hashCode = sha256Hash.digest('hex');
+
 };
 
 await calculateHash();
