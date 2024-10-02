ada0339de0c30160968410468dc8b716b933bccc
Igor Checheurov <chei@yandex.ru>
2024-10-02 14:00:08 +0300

    feat: add function parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    let varKeys = Object.keys(process.env).filter(envVar => envVar.startsWith('RSS_'));
+    let result = [];
+    for(let key of varKeys) {
+        result.push(key + '=' + process.env[key]);
+    }
+
+    console.log(result.join(', '));
 };
 
 parseEnv();
