6070abcdcb1fd34489847246ac17587b2988b314
Igor Checheurov <chei@yandex.ru>
2024-10-03 16:42:18 +0300

    feat: add compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+const zlib = require('zlib');
+const { pipeline } = require('stream');
+const fs = require('fs');
+
+const { promisify } = require('util');
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    async function do_gzip(input, output) {
+        const gzip = zlib.createGzip();
+        const origFile = fs.createReadStream(input);
+        const resFile = fs.createWriteStream(output);
+        await pipe(origFile, gzip, resFile);
+    }
+
+    do_gzip('files/fileToCompress.txt', 'files/archive.gz')
+    .catch((err) => {
+      console.error('An error occurred:', err);
+      process.exitCode = 1;
+    });    
 };
 
 await compress();
+
+
+    
+
+  
+
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,7 @@
+const zlib = require('zlib');
+const stream = require('stream');
+const fs = require('fs');
+
 const decompress = async () => {
     // Write your code here 
 };
