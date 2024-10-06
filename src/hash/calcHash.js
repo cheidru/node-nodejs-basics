4ddb469a20ee4ed752be9395c3792b6b6925d301
Igor Checheurov <chei@yandex.ru>
2024-10-06 14:42:12 +0300

    fix: fix bugs of hash module func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,13 +1,6 @@
-const crypto = require('crypto');
-const fs = require('fs');
-const stdout = require('process');
-
-// const stream = require('stream');
-
-// https://www.techiedelight.com/generate-sha-256-hash-javascript/
-// https://nodejs.org/dist/latest-v22.x/docs/api/crypto.html#cryptocreatehashalgorithm-options
-// https://nodejs.org/dist/latest-v22.x/docs/api/crypto.html#hashupdatedata-inputencoding
-// https://nodejs.org/api/stream.html
+import crypto from 'crypto';
+import fs from 'fs';
+import process from 'process';
 
 const calculateHash = async () => {
     // Write your code here
@@ -16,34 +9,9 @@ const calculateHash = async () => {
     fs.readFile('files/fileToCalculateHashFor.txt', (err, data) => {
         if(err) throw err;
         sha256Hash.update(data);
-    })    
-
-    sha256Hash.digest('hex').stdout;
-    // const writableStream = new stream.Writable();
-    // writableStream._write = (hashCode, encoding, next) => {
-        // console.log(hashCode.toString());
-    //     next();
-    // }
+        process.stdout.write(sha256Hash.digest('hex'))
+        // console.log(sha256Hash.digest('hex'));
+    })
 };
 
 await calculateHash();
-
-// https://blog.dennisokeeffe.com/blog/2024-07-12-transform-streams-in-nodejs%20copy
-//
-// const crypto = require("node:crypto");
-// const fs = require("node:fs");
-// 
-// const algorithm = "aes-192-cbc";
-// const password = "Password used to generate key";
-// 
-// const key = crypto.scryptSync(password, "salt", 24);
-// const iv = Buffer.alloc(16, 0);
-// 
-// const cipher = crypto.createCipheriv(algorithm, key, iv);
-// 
-// const input = fs.createReadStream("input.txt");
-// const output = fs.createWriteStream("encrypted.txt");
-// 
-// input.pipe(cipher).pipe(output);
-// 
-// output.on("finish", () => console.log("File encrypted successfully"));
