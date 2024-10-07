8218218ccacfdbeeaad8b53f921e00229b502071
Igor Checheurov <chei@yandex.ru>
2024-10-07 14:32:29 +0300

    feat: start coding child process functions

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import fork from 'child_process';
+import process from 'process';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const child = fork('/files/script.js', process.argv);
+    // child.send(process.stdin.pipe(child));
+    process.stdin.on('data', (data) => {child.send(data)});
+    child.on('data', (data) => {process.stdout.write(data)});
 };
 
 // Put your arguments in function call to test this functionality
