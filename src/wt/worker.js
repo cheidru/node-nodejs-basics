7ac99c96efd46d1f545534c67c349b4a9be59e2d
Igor Checheurov <chei@yandex.ru>
2024-10-07 08:51:14 +0300

    feat: add error handling to worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -4,9 +4,15 @@ import { workerData, parentPort } from 'worker_threads';
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const result = nthFibonacci(workerData);
-    // This function sends result of nthFibonacci computations to main thread
-    parentPort.postMessage({ result, status: 'resolved' });
+    try {
+        const result = nthFibonacci(workerData);
+        // This function sends result of nthFibonacci computations to main thread
+        parentPort.postMessage({ result, status: 'resolved' });
+    } catch (err) {
+        result = null;
+        parentPort.postMessage({ result, status: 'error' });
+    }
+
 };
 
 sendResult();
